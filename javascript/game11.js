e974c9b24b5bb950b4e17926133e12a541a33642
João Victor <joaovictor-san-co@hotmail.com>
2020-07-02 21:51:49 -0300

    game11 alpha

--- javascript/game11.js
+++ javascript/game11.js
@@ -2,12 +2,13 @@ var inicioGame = false
 var fimGame = false
 var sttComecar = false
 var penultimoNum = parseInt(gerarRandom())
+var penultimoNumR = penultimoNum
 var ultimoNum = parseInt(gerarRandom())
 var ultimoNumR = ultimoNum
 var posicaoRandom = Math.random() * 10
 var maxRandom = penultimoNum + ultimoNum + Math.random() * 15
 var minRandom = penultimoNum + ultimoNum - Math.random() * 15
-var counterIdDivCerto = 0
+var counterIdDivCerto = 1
 var counterIdDivErrado = -1
 var respostaRandom = parseInt(Math.random() * (maxRandom - minRandom) + minRandom)
 if(respostaRandom == penultimoNum + ultimoNum) {
@@ -15,10 +16,10 @@ if(respostaRandom == penultimoNum + ultimoNum) {
 }
 function comecar() {
     document.querySelector('#botaoIniciar').innerHTML = penultimoNum
-    document.querySelector('#segundoNumero').innerHTML = ultimoNum
+    document.getElementById('0').innerHTML = ultimoNum
     iniciarCronometro()
     criarDivCertoErrado()
-    
+    document.getElementById('0').onclick = ''
 }
 function criarDivCertoErrado() {
     if(posicaoRandom < 5) {
@@ -32,7 +33,7 @@ function criarDivCertoErrado() {
 }
 function mudarValores() {
     posicaoRandom = Math.random() * 10
-    penultimoNum = ultimoNumR - penultimoNum
+    penultimoNum = penultimoNumR
     ultimoNum = ultimoNumR
     maxRandom = penultimoNum + ultimoNum + Math.random() * 15
     minRandom = penultimoNum + ultimoNum - Math.random() * 15
@@ -52,19 +53,21 @@ function criarDiv(arg) {
         if(posicaoRandom > 5) {
             document.querySelector('#conteudo').appendChild(document.createElement('br'))
         }
+        penultimoNumR = Number(document.getElementById(`${counterIdDivCerto-1}`).innerHTML)
         document.getElementById(`${counterIdDivCerto}`).innerHTML = penultimoNum + ultimoNum
-        document.getElementById(`${counterIdDivCerto}`).style.background = 'rgba(0,0,0,0.7)'
         console.log(penultimoNum)
         console.log(ultimoNum)
         ultimoNumR = Number(document.getElementById(`${counterIdDivCerto}`).innerHTML)
-        
+        document.getElementById(`${counterIdDivCerto-1}`).removeEventListener("click", criarDivCertoErrado)
+        document.getElementById(`${counterIdDivCerto-1}`).removeEventListener("click", mudarValores)
+        document.getElementById(`${counterIdDivCerto-1}`).style.background = 'rgb(64,64,64)'
         counterIdDivCerto++
         
     } else {
         var createDiv = document.createElement('div')
         createDiv.id = counterIdDivErrado
         createDiv.className = 'alternativa'
-        createDiv.addEventListener("click", pararJogo)
+        createDiv.addEventListener("mousedown", pararJogo)
         document.querySelector('#conteudo').appendChild(createDiv)
         if(posicaoRandom < 5) {
             document.querySelector('#conteudo').appendChild(document.createElement('br'))
@@ -75,10 +78,21 @@ function criarDiv(arg) {
 }
 function iniciarCronometro() {
     inicioGame = Date.now()
+    var sec = 0
+    setInterval(() => {
+        document.querySelector('#cronometro').innerHTML = sec
+        sec ++
+    }, 1000);
 }
 function iniciarGame() {
-    document.querySelector('#segundoNumero').innerHTML = ' '
-    document.querySelector('#segundoNumero').style.display = 'inline-block'
+    document.getElementById('0').style.display = 'inline-block'
+    document.getElementById('0').style.padding = '15px'
+    document.getElementById('0').style.background = 'rgb(0, 202, 0)'
+    document.getElementById('0').style.boxShadow = '0px 0px 10px green'
+    document.getElementById('0').style.borderRadius = '5px'
+    document.getElementById('0').style.margin = '5px'
+    document.getElementById('0').style.fontFamily = 'Arial, Helvetica, sans-serif'
+    document.querySelector('#botaoIniciar').onclick = ' '
 }
 function gerarRandom() {
     return Math.random() * 10 * (Math.random() * 10)
